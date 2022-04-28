e853d3e481a15c6c4677cba1decad1152e5405a2
F-balanta <jf.balanta@hotmail.com>
2022-04-28 00:47:00 -0500

    Adding Documentations

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,6 +2,10 @@
 
 namespace MyMath
 {
+    ///<summary>This class contains methods matemathics</summary>
+    /// <param name="a"> This param is the first argument to evaluate
+    /// <param name="b"> This param is the second argument to evaluate
+    /// <return> return the add of two integer</return>
     public class Operations
     {
         public static int Add(int a, int b)
