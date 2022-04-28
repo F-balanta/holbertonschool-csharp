48e6ee9a3ced0538f704c0d1e384ff9ba0e50064
F-balanta <jf.balanta@hotmail.com>
2022-04-28 18:44:30 -0500

    Adding comments

--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -3,8 +3,16 @@ using System;
 
 namespace Text
 {
+    /// <summary>
+    /// Vefiry if an string is a palindrome
+    /// </summary>
     public class Str
     {
+        /// <summary>
+        /// Vefiry if an string is a palindrome
+        /// </summary>
+        /// <param name="s"> string to evaluate</param>
+        /// <returns>True or False</returns>
         public static bool IsPalindrome(string s)
         {
             if (s.Length == 0)
