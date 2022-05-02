90b0be0128ea42dc56a7aa1f2d9aa047f18ea74c
F-balanta <jf.balanta@hotmail.com>
2022-05-02 17:42:49 -0500

    Get name of properties ans methods

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-
+using System.Reflection;
+/// <summary>
+/// OBJ Class
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// Method Print Properties and Methods available of an object
+    /// </summary>
+    /// <param name="myObj"></param>
     public static void Print(object myObj)
     {
-        Console.WriteLine($"{myObj.GetType().Name} Properties:");
-        foreach (var element in myObj.GetType().GetProperties())
-            Console.WriteLine($"{element.Name}");
+        TypeInfo tobj = myObj.GetType().GetTypeInfo();
+        Console.WriteLine($"{tobj.Name} properties:");
+        foreach (PropertyInfo properties in tobj.GetProperties())
+            Console.WriteLine(properties.Name);
 
-        Console.WriteLine($"{myObj.GetType().Name} Methods");
-        foreach (var elem in myObj.GetType().GetMethods())
-            Console.WriteLine($"{elem.Name}");
+        Console.WriteLine("{0} Methods:", tobj.Name);
+        foreach (MethodInfo methods in tobj.GetMethods())
+            Console.WriteLine(methods.Name);
     }
 }
