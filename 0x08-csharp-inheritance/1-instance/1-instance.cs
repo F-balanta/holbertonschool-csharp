3c4daff09ef5ed6f887b405d89a224f08dd8ec28
F-balanta <jf.balanta@hotmail.com>
2022-05-02 12:23:18 -0500

    Array instance of?

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType() == typeof(int[]))
+            return true;
+        else
+            return false;
+    }
+}
