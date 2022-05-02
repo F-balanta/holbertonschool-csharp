87dff919c01e5d4561513a18f88f51fab4075dbd
F-balanta <jf.balanta@hotmail.com>
2022-05-02 13:30:59 -0500

    Adding condition

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -11,6 +11,8 @@ class Obj
     /// <returns>return true if obj is an object of type Int otherwise return false</returns>
     public static bool IsOfTypeInt(object obj)
     {
+        if (obj == null)
+            return false;
         if (obj.GetType() == typeof(int))
             return true;
         else
