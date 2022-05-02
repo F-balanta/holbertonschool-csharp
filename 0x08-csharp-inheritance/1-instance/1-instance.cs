7b48dd501bc80c61670dc951bdd4907ebf73a6ab
F-balanta <jf.balanta@hotmail.com>
2022-05-02 13:01:01 -0500

    Adding comments

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-
+/// <summary>
+///  This class contains one method public
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// This method returns true if is object
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns>returns true if is object otherwise returns false</returns>
     public static bool IsInstanceOfArray(object obj)
     {
         if (obj == null)
