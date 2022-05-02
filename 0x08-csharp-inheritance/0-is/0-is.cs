91538bdcf0228e442a0b800596998045ec036bb4
F-balanta <jf.balanta@hotmail.com>
2022-05-02 13:02:29 -0500

    Adding comments

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,14 @@
 ﻿using System;
-
+/// <summary>
+/// This class contains one method public
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// This method return tre if obj is an object of type Int 
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns>return true if obj is an object of type Int otherwise return false</returns>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj.GetType() == typeof(int))
