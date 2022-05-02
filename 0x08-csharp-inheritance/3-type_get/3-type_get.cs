8d249c4f2823d43650db38f75a2075739978c494
F-balanta <jf.balanta@hotmail.com>
2022-05-02 16:12:40 -0500

    Type Get

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (var element in myObj.GetType().GetProperties())
+            Console.WriteLine($"{element.Name}");
+        Console.WriteLine($"{myObj.GetType().Name} Methods");
+        foreach (var elem in myObj.GetType().GetMethods())
+            Console.WriteLine($"{elem.Name}");
+    }
+}
