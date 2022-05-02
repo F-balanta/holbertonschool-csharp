ef5a0f908dc0e5ade1f04ad502f93615cf85a1ec
F-balanta <jf.balanta@hotmail.com>
2022-05-02 14:41:57 -0500

    Adding Documentation Line

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if(derivedType is baseType)
+            return true;
+    }
+}
