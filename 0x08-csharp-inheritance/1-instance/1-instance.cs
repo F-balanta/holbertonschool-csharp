3af193e70c78a64418ba2e2aa839251a4770b101
F-balanta <jf.balanta@hotmail.com>
2022-05-02 12:35:25 -0500

    Update task

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -5,7 +5,9 @@ class Obj
 {
     public static bool IsInstanceOfArray(object obj)
     {
-        if (obj.GetType() == typeof(int[]))
+        if (obj == null)
+            return false;
+        if (obj.GetType() == typeof(Int32[]))
             return true;
         else
             return false;
