47e907fadca1f03187f60826ecea3211f7abfdda
F-balanta <jf.balanta@hotmail.com>
2022-05-23 20:36:06 -0500

    Update task

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -14,11 +14,11 @@ class VectorMath
     {
         if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
             return -1;
-        double result = 0;
+        double result = 0.0;
         for (int i = 0; i < vector.GetLength(0); i++)
         {
-            result += Math.Round(Math.Sqrt(Math.Pow(vector[i],2)));
+            result += Math.Pow(vector[i],2);
         }
-        return result;
+        return Math.Round(Math.Sqrt(result),2);
     }
 }
