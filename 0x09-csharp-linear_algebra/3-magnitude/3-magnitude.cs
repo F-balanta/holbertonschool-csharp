c2ccb0256fe6f93147c26c7c68df01e6ffcf9e3b
F-balanta <jf.balanta@hotmail.com>
2022-05-23 20:32:04 -0500

    Adding comments

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 
-
+/// <summary>
+/// This class containing a public method that calculate the length of a given vector.
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// Return the Long of a given vector
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <returns>The return value should be rounded to the nearest hundredth</returns>
     public static double Magnitude(double[] vector)
     {
         if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
