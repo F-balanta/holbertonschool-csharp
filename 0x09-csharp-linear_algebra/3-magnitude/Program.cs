f3b107dad9a5b9a150e6bb0c0149809f3cf00e64
F-balanta <jf.balanta@hotmail.com>
2022-05-23 20:16:33 -0500

    Upload file .Gitignore and solved task 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
+            return -1;
+        double result = 0;
+        for (int i = 0; i < vector.GetLength(0); i++)
+        {
+            result += Math.Round(Math.Sqrt(Math.Pow(vector[i],2)));
+        }
+        return result;
+    }
+}
